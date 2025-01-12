d8f3389676ed08b2f2b3c896359c2b2a71c1c50c
iriquelmee <i.riquelmee@duocuc.cl>
2025-01-12 19:50:24 -0300

    Add ruteo basico vistas:  DashboardComponent, PacienteComponent, SoporteComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,30 @@ import { Routes } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { ApiTestComponent } from './components/api-test/api-test.component';
 import { AppComponent } from './app.component';
+import { PacienteComponent } from './components/paciente/paciente.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SoporteComponent } from './components/soporte/soporte.component';
 
 export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'/dashboard', 
+        pathMatch:'full'},
     {
         path: 'apitest',
         component: ApiTestComponent,
         canActivate: [MsalGuard],
-      }
+    },
+    {
+      path: 'dashboard',
+      component: DashboardComponent,
+    },
+    {
+      path: 'paciente',
+      component: PacienteComponent,
+    },
+    {
+      path: 'soporte',
+      component: SoporteComponent,
+    }
 ];
