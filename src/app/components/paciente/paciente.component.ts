f45d09cebf90e999d301807c9b4169be98f3e87c
iriquelmee <i.riquelmee@duocuc.cl>
2025-01-12 19:44:52 -0300

    Add componente modulo pacientes mas estilos basicos

---
+++ src/app/components/paciente/paciente.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './paciente.component.html',
+  styleUrl: './paciente.component.css'
+})
+export class PacienteComponent {
+
+  title:string = "Modulo Pacientes";
+  subtitle:string = "Bienvenido al módulo de Gestión de Pacientes Aquí podrás gestionar los registros de pacientes y otras tareas relacionadas.";
+
+}
